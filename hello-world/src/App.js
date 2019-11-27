fd493ace190f98dc15e3ccf9cdd8959e1791fba3
norris <norris@gmail.com>
2019-11-26 21:41:28 -0500

    Greet2

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hi from './components/Hi'
+import Greet2 from './components/Greet2'
 function App() {
   return (
     <div className="App">
-      {/*<Greet /> */}
-      {/*<Welcome /> */}
-      <Hi />
+      <Greet2 name="user1" /> 
+      <Greet2 name="user2" /> 
+      {/*<Welcome />*/}
+      { /*<Hi />*/}
     </div>
   );
 }
---
+++ hello-world/src/components/Greet2.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Greet2 = props => {
+    console.log(props)
+return <h1>Hello {props.name}</h1>
+}
+
+export default Greet2
